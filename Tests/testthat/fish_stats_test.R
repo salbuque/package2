4f0636c42b771fd74212f24528d2be3b952d600b
salbuque <salbuquerque@ucsb.edu>
2021-03-16 11:41:46 -0700

    try numeric expect_equal()

--- Tests/testthat/fish_stats_test.R
+++ Tests/testthat/fish_stats_test.R
@@ -1,5 +1,6 @@
 test_that("fish_stats_Works", {
 fake_fish_data<- as.data.frame(x = as_factor(c("ray", "salmon", "seabass", "seabass", "ray", "goldfish", "goldfish", "ray")))
 
+expect_equal(fish_stats(fake_fish_data, graph = FALSE)$total_fish, 8)
 expect_match(fish_stats(fake_fish_data, graph = FALSE)$most_common, "ray", fixed = TRUE)
 })
