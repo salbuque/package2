333b9c383684d86e5a0ca6d4b041d7c334bb4c17
salbuque <salbuquerque@ucsb.edu>
2021-03-16 12:05:00 -0700

    back to basics!

--- Tests/testthat/fish_stats_test.R
+++ Tests/testthat/fish_stats_test.R
@@ -1,6 +1,8 @@
 test_that("fish_stats_Works", {
-fake_fish_data<- as.data.frame(x = as_factor(c("ray", "salmon", "seabass", "seabass", "ray", "goldfish", "goldfish", "ray")))
+
+fake_fish_data<- c("ray", "salmon", "seabass", "seabass", "ray", "goldfish", "goldfish", "ray")
 
 expect_equal(fish_stats(fake_fish_data, graph = FALSE)$total_fish, 8)
-expect_match(fish_stats(fake_fish_data, graph = FALSE)$most_common, "ray", fixed = TRUE)
+expect_match(fish_stats(fake_fish_data, graph = FALSE)$rarest_fish, "salmon")
+expect_match(fish_stats(fake_fish_data, graph = FALSE)$most_common, "ray")
 })
