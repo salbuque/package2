1d7cf777d306ecfb426e5d84ea778d1c011e60a9
salbuque <salbuquerque@ucsb.edu>
2021-03-16 12:16:37 -0700

    loaded

--- Tests/testthat/fish_stats_test.R
+++ Tests/testthat/fish_stats_test.R
@@ -2,7 +2,7 @@ test_that("fish_stats_Works", {
 
 fake_fish_data<- c("ray", "salmon", "seabass", "seabass", "ray", "goldfish", "goldfish", "ray")
 
-expect_equal(as.numeric(fish_stats(fake_fish_data, graph = FALSE)$total_fish), 8)
-expect_match(as.character(fish_stats(fake_fish_data, graph = FALSE)$rarest_fish), "salmon")
-expect_match(as.character(fish_stats(fake_fish_data, graph = FALSE)$most_common), "ray")
+expect_equal(fish_stats(fake_fish_data, graph = FALSE)$total_fish, 8)
+expect_match(fish_stats(fake_fish_data, graph = FALSE)$rarest_fish, "salmon")
+expect_match(fish_stats(fake_fish_data, graph = FALSE)$most_common, "ray")
 })
