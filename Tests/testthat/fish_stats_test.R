2a0127fde8d44866da04d29c50108a6f81de2c55
salbuque <salbuquerque@ucsb.edu>
2021-03-16 11:29:17 -0700

    expect_match for strings, called function fish_stats

--- Tests/testthat/fish_stats_test.R
+++ Tests/testthat/fish_stats_test.R
@@ -1,7 +1,7 @@
-test_that("fish_stats_test", {
+test_that("fish_stats_Works", {
 fake_fish_data<- as.data.frame(x = c("ray", "salmon", "seabass", "seabass", "ray", "goldfish", "goldfish", "ray"))
 
-expect_equal(most_common == "ray")
-expect_equal(rarest_fish == "salmon")
-expect_equal(total_fish == 8)
+expect_match(fish_stats(fake_fish_data, graph = false)$most_common, "ray")
+expect_match(fish_stats(fake_fish_data, graph = false)$rarest_fish, "salmon")
+expect_equal(fish_stats(fake_fish_data, graph = false)$total_fish, 8)
 })
